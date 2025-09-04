62e0f0fbc4bb44ae6ca3787597eb674a6c1fda66
FacundoBe <facundobellomo@gmail.com>
2025-09-04 16:30:01 -0300

    primer bosquejo fondo y logo

--- script.js
+++ script.js
@@ -10,6 +10,35 @@ gsap.ticker.add(time => {
 })
 gsap.ticker.lagSmoothing(0)
 
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top top",
+    end: "+=600",
+    scrub: 0.8,
+    markers: false,
+  },
+})
+
+tl.to(".tunel",{scale:1.15, duration:6.5})
+.to(".ojos",{
+  opacity:1,
+  scale:1.25,
+  duration:1.0,
+},"<")
+.to(".ojos",{
+  opacity:0,
+  duration:0.5,
+}, ">")
+.to('.logo',{
+  opacity:1,
+  duration:1,
+},"<0.5")
+.to('.logo',{
+  scale:30,
+  ease:"power2.in",
+  duration:5,
+},"<")
 
 
 
