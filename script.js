ec3c4c0bd965f10a49d5e0a867b6e44f6b223cf6
FacundoBe <facundobellomo@gmail.com>
2025-09-04 19:37:25 -0300

    simplificada la animacion original

--- script.js
+++ script.js
@@ -14,28 +14,20 @@ const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".hero",
     start: "top top",
-    end: "+=600",
-    scrub: 0.8,
+    end: "+=1200",
+    scrub: 1,
     markers: false,
   },
 })
 
-tl.to(".tunel",{scale:1.15, duration:6.5})
-.to(".ojos",{
-  opacity:1,
-  scale:1.25,
-  duration:1.0,
-},"<")
-.to(".ojos",{
-  opacity:0,
-  duration:0.5,
-}, ">")
+tl.to(".tunel",{scale:5,y:-300, opacity:0, ease:"power1.in", duration:8})
 .to('.logo',{
   opacity:1,
   duration:1,
 },"<0.5")
 .to('.logo',{
-  scale:30,
+  scale:35,
+  y:-100,
   ease:"power2.in",
   duration:5,
 },"<")
